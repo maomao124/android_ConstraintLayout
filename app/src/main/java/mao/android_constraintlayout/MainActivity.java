15dd0d627335a12ba6acf963328ebd4faf17e14a
mao <1296193245@qq.com>
2022-10-07 14:12:24 +0800

    android_ConstraintLayout

--- app/src/main/java/mao/android_constraintlayout/MainActivity.java
+++ app/src/main/java/mao/android_constraintlayout/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                startActivity(new Intent(MainActivity.this, MainActivity8.class));
+                startActivity(new Intent(MainActivity.this, MainActivity10.class));
             }
         });
         findViewById(R.id.Button10).setOnClickListener(new View.OnClickListener()
